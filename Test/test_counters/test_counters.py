5fc22e148f1e3206b7fef3c837dd158b51966f10
Feiwen JIA <fjia@aneo.fr>
2020-04-17 12:01:31 +0200

    change test structure

--- Test/test_counters/test_counters.py
+++ Test/test_counters/test_counters.py
@@ -57,7 +57,25 @@ class flag_tests(unittest.TestCase):
     def test_inactive(self):
         """Test that counter is not displayed with the flag is equal to 0"""
         self.compareOutputWithReference([self.pinpath, '-t', self.toolpath, '-counter-mode', '0', '--', self.execpath], "test_counters_reference_inactive.txt")
+
+class count_tests(unittest.TestCase):
+    workpath = "@COUNTERS_TESTS_BINARY_DIR@"
+    execpath = "@COUNTERS_TESTS_ECEXUTABLE@"
+    toolpath = "@COUNTERS_TESTS_PINTOOL@"
+    pinpath = "@PIN_EXECUTABLE@"
         
+
+    def compareOutputWithReference(self, runargs, referenceFileName):
+        out = subprocess.run(runargs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell = True)
+        output = out.stdout.decode('utf-8')
+        splittedOutput = output.splitlines()
+        print(output) 
+        with open(self.workpath + '/' + referenceFileName) as ref:
+            result = ref.read()
+            result = result.splitlines()
+        self.assertEqual(splittedOutput,result)
+
+       
     def test_add_float_scalar(self):
         """Test the validity of couting add_float_scalar"""
         self.compareOutputWithReference([self.pinpath, '-t', self.toolpath, '-counter-mode', '1', '--', self.execpath, "add", "float", "scalar", "10", "0", "0"], "test_counters_reference_add_float_scalar.txt")
