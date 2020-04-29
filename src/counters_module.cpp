5c5a088dab78c4f712bd45318ba3d6448407d5a0
Wilfried KIRSCHENMANN <wkirschenmann@aneo.fr>
2020-04-29 09:37:41 +0200

    * Simplify counter_module code for TLS Handling

--- src/counters_module.cpp
+++ src/counters_module.cpp
@@ -4,10 +4,14 @@
 #include <pin.H>
 #include <iostream>
 #include <cassert>
+#include "utils.h"
 
 namespace pene {
+  using namespace utils;
   namespace counter_module_internals {
-    static  TLS_KEY tls_counters_key = INVALID_TLS_KEY;
+
+    static TLS_KEY tls_counters_key = INVALID_TLS_KEY;
+    static REG tls_counter_reg;
 
     void init_tls() {
       tls_counters_key = PIN_CreateThreadDataKey(NULL);
@@ -15,6 +19,13 @@ namespace pene {
       {
         std::cerr << "number of already allocated keys reached the MAX_CLIENT_TLS_KEYS limit(" << MAX_CLIENT_TLS_KEYS << ")" << std::endl;
         PIN_WriteErrorMessage("number of already allocated keys reached the MAX_CLIENT_TLS_KEYS limit", 1000, PIN_ERR_SEVERITY_TYPE::PIN_ERR_FATAL, 0);
+        PIN_ExitProcess(1);
+      }
+      tls_counter_reg = PIN_ClaimToolRegister();
+      if (!REG_valid_for_iarg_reg_value(tls_counter_reg))
+      {
+        std::cerr << "Cannot allocate tls register for counters" << std::endl;
+        PIN_WriteErrorMessage("Cannot allocate tls register for counters", 1000, PIN_ERR_SEVERITY_TYPE::PIN_ERR_FATAL, 0);
         PIN_ExitProcess(1);
       }
     }
@@ -53,95 +64,66 @@ namespace pene {
       *a += b;
     }
 
-    template<class ... Types> 
-    struct addn {};
-
-    template<class ... Types> 
-    struct addn<UINT32, UINT32, Types...> {
-      inline static void apply(counters* c, UINT32 idx, UINT32 b, Types ... values)
-      {
-        c->array[idx] += b;
-        addn<Types...>::apply(c, values...);
-      }
-    };
-
-    template<> 
-    struct addn<UINT32, UINT32> {
-      inline static void apply(counters* c, UINT32 idx, UINT32 b)
-      {
-        c->array[idx] += b;
-      }
-    };
-
-    template<class ... Types> 
-    void PIN_FAST_ANALYSIS_CALL Addn_tls(THREADID threadid, Types ... values)
+    static counters* PIN_FAST_ANALYSIS_CALL get_counters_tls(THREADID threadid)
     {
-      counters* c = static_cast<counters*>(PIN_GetThreadData(tls_counters_key, threadid));
-      addn<Types...>::apply(c, values...);
+      return static_cast<counters*>(PIN_GetThreadData(tls_counters_key, threadid));
     }
 
-    template <class A, class B> struct is_same { static constexpr bool value = false; };
-    template <class A> struct is_same<A, A> { static constexpr bool value = true; };
-
-    template<class LOC, int N> struct counters_tls_instrumenter
+    template<int I>
+    static void PIN_FAST_ANALYSIS_CALL Addi(void* voided_counters, UINT32 b)
     {
-      template <class ... Types> struct  helper
-      {
-        template<class ... Args>
-        static void apply(LOC loc, UINT32* indexes, UINT32* values, Args... args)
-        {
-          if constexpr (N == 0)
-          {
-            indexes; //suppress C4100 warning with VS
-            values; //suppress C4100 warning with VS
-            if constexpr (is_same<LOC, BBL>::value)
-            {
-              BBL_InsertCall(loc, IPOINT_ANYWHERE, (AFUNPTR)Addn_tls<Types...>, IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID, args..., IARG_END);
-              return;
-            }
-            else if constexpr (is_same<LOC, INS>::value)
-            {
-              INS_InsertCall(loc, IPOINT_BEFORE, (AFUNPTR)Addn_tls<Types...>, IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID, args..., IARG_END);
-              return;
-            }
-            else static_assert(false);
-          }
-          else if constexpr (N > 0)
-          {
-            counters_tls_instrumenter<LOC, N - 1>::helper<UINT32, UINT32, Types...>::apply(
-              loc, indexes, values, IARG_UINT32, indexes[N - 1], IARG_UINT32, values[N - 1], args...);
-          }
-          else static_assert(false);
-        }
-      };
+      volatile auto c = static_cast<counters*>(voided_counters);
+      Add(c->array + I, b);
+    }
 
-      template <class LOC>
-      static void apply(LOC loc, UINT32* indexes, UINT32* values)
-      {
-        static_assert(N > 0);
-        counters_tls_instrumenter<LOC, N - 1>::helper<UINT32, UINT32>::apply(
-          loc, indexes, values, IARG_UINT32, indexes[N - 1], IARG_UINT32, values[N - 1]);
-      }
-    };
-
-    template <class LOC, int N=1>
-    int add_counters_tls(LOC loc, UINT32* indexes, UINT32* values, UINT32 nb_elts)
+    template <class LOC, int N=0>
+    void add_counters_tls(LOC loc, IPOINT ipoint, UINT32 index, UINT32 value)
     {
-      if (nb_elts < N)
+      if (index < N)
       {
-        std::cerr << "ERROR: nb_elts(" << nb_elts << ")<N(" << N << ")\n";
-        PIN_WriteErrorMessage("ERROR: nb_elts<N)", 1000, PIN_ERR_FATAL, 0);
+        std::cerr << "ERROR: index(" << index << ")<N(" << N << ")\n";
+        PIN_WriteErrorMessage("ERROR: index<N)", 1000, PIN_ERR_FATAL, 0);
         PIN_ExitApplication(-1);
       }
       if constexpr(N < counters::size)
       {
-        if (nb_elts > N)
+        if (index > N)
         {
-          return add_counters_tls<LOC, N + 1>(loc, indexes, values, nb_elts);
+          add_counters_tls<LOC, N + 1>(loc, ipoint, index, value);
+          return;
         }
       }
-      counters_tls_instrumenter<LOC, N>::apply(loc, indexes, values);
-      return N;
+      insertCall(loc, ipoint, (AFUNPTR)Addi<N>, IARG_FAST_ANALYSIS_CALL, IARG_REG_VALUE, tls_counter_reg, IARG_UINT32, value, IARG_END);
+    }
+
+    template <class LOC>
+    static void counters_instrumentation(LOC loc, IPOINT ipoint, counters& c, const counters& tmp)
+    {
+      if (tls_counters_key == INVALID_TLS_KEY)
+      {
+        for (UINT32 i = 0; i < counters::size; ++i)
+        {
+          if (tmp.array[i] > 0)
+          {
+            insertCall(loc, ipoint, (AFUNPTR)Add, IARG_FAST_ANALYSIS_CALL, IARG_PTR, &(c.array[i]), IARG_UINT32, (UINT32)tmp.array[i], IARG_END);
+          }
+        }
+      }
+      else
+      {
+        bool is_init = false;
+        for (UINT32 i = 0; i < counters::size; ++i)
+        {
+          if (tmp.array[i] > 0)
+          {
+            if (!is_init)
+            {
+              insertCall(loc, ipoint, (AFUNPTR)get_counters_tls, IARG_FAST_ANALYSIS_CALL, IARG_THREAD_ID, IARG_RETURN_REGS, tls_counter_reg, IARG_END);
+            }
+            add_counters_tls(loc, ipoint, i, (UINT32)tmp.array[i]);
+          }
+        }
+      }
     }
 
     static void INS_CountersInstrumentation(INS ins, void* void_counters)
@@ -152,35 +134,8 @@ namespace pene {
         auto tmp_counters = counters();
         auto oc = INS_Opcode(ins);
         update_counters(oc, tmp_counters);
-        if (tls_counters_key == INVALID_TLS_KEY)
-        {
-          for (UINT i = 0; i < counters::size; ++i)
-          {
-            if (tmp_counters.array[i] > 0)
-            {
-              INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)Add, IARG_FAST_ANALYSIS_CALL, IARG_PTR, &(counters_.array[i]), IARG_UINT32, (UINT32)tmp_counters.array[i], IARG_END);
-            }
-          }
-        }
-        else
-        {
-          UINT32 indexes[counters::size];
-          UINT32 values[counters::size];
-          UINT32 nb_elts = 0;
-          for (UINT i = 0; i < counters::size; ++i)
-          {
-            if (tmp_counters.array[i] > 0)
-            {
-              indexes[nb_elts] = i;
-              values[nb_elts] = (UINT32)tmp_counters.array[i];
-              ++nb_elts;
-            }
-          }
-          if (nb_elts>0)
-          {
-            nb_elts -= add_counters_tls<INS>(ins, indexes, values, nb_elts);
-          }
-        }
+
+        counters_instrumentation(ins, IPOINT_BEFORE, counters_, tmp_counters);
       }
     }
 
@@ -201,35 +156,7 @@ namespace pene {
           }
         }
 
-        if (tls_counters_key == INVALID_TLS_KEY)
-        {
-          for (UINT i = 0; i < counters::size; ++i)
-          {
-            if (tmp_counters.array[i] > 0)
-            {
-              BBL_InsertCall(bbl, IPOINT_ANYWHERE, (AFUNPTR)Add, IARG_FAST_ANALYSIS_CALL, IARG_PTR, &(counters_.array[i]), IARG_UINT32, (UINT32)tmp_counters.array[i], IARG_END);
-            }
-          }
-        }
-        else
-        {
-          UINT32 indexes[counters::size];
-          UINT32 values[counters::size];
-          UINT32 nb_elts = 0;
-          for (UINT i = 0; i < counters::size; ++i)
-          {
-            if (tmp_counters.array[i] > 0)
-            {
-              indexes[nb_elts] = i;
-              values[nb_elts] = (UINT32)tmp_counters.array[i];
-              ++nb_elts;
-            }
-          }
-          if (nb_elts > 0)
-          {
-            nb_elts -= add_counters_tls<BBL>(bbl, indexes, values, nb_elts);
-          }
-        }
+        counters_instrumentation(bbl, IPOINT_ANYWHERE, counters_, tmp_counters);
       }
     }
   }
