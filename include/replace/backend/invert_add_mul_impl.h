27f3a8e13f4465edce6500d020ace7da5c48ef56
Wilfried KIRSCHENMANN <wkirschenmann@aneo.fr>
2020-05-05 00:17:19 +0200

    * fix replace tests
    Note : the print function uses double precision arithmetics. Modyfying double precision behaviour leads to wrong behaviours in these parts.

--- include/replace/backend/invert_add_mul_impl.h
+++ include/replace/backend/invert_add_mul_impl.h
@@ -12,9 +12,9 @@ namespace pene
         static constexpr void mul_float(float a, float b, float* cptr, void*) noexcept { *cptr = a + b; }// replace * with +
         static constexpr void div_float(float a, float b, float* cptr, void*) noexcept { *cptr = a / b; }
 
-        static constexpr void add_double(double a, double b, double* cptr, void*) noexcept { *cptr = a * b; }// replace + with *
+        static constexpr void add_double(double a, double b, double* cptr, void*) noexcept { *cptr = a + b; }
         static constexpr void sub_double(double a, double b, double* cptr, void*) noexcept { *cptr = a - b; }
-        static constexpr void mul_double(double a, double b, double* cptr, void*) noexcept { *cptr = a + b; }// replace * with +
+        static constexpr void mul_double(double a, double b, double* cptr, void*) noexcept { *cptr = a * b; }
         static constexpr void div_double(double a, double b, double* cptr, void*) noexcept { *cptr = a / b; }
       };
     }
