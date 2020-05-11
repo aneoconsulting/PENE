b2380ea51c1aad2d7351bda53d2216c268364fd5
Feiwen JIA <fjia@aneo.fr>
2020-05-11 17:03:41 +0200

    putting condition on counter displaying, for simplification purpose, print only non zero line.

--- src/counters.cpp
+++ src/counters.cpp
@@ -3,14 +3,14 @@
 #include <iostream>
 #include <iomanip>
 
-#define PRINT(prec, op, mode,ins)                                                           \
+#define PRINT(prec, op, mode,ins) ((*this)[counter_type::##op##_##prec##_##mode##_##ins] ? \
 std::cerr                                                                                   \
           << std::setw(17) << std::left << #prec                                            \
           << std::setw(17) << std::left << #op                                              \
           << std::setw(17) << std::left << #mode                                            \
           << std::setw(17) << std::left << #ins                                            \
           << std::setw(17) << std::right << (*this)[counter_type::##op##_##prec##_##mode##_##ins]   \
-          << std::endl
+          << std::endl : std::cerr << "")
 
 
 namespace pene {
