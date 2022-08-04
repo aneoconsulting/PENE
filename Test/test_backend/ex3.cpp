9956882bd8093b477b1ee0345417cb5ade2bb9e6
melflitty <melflitty@aneo.fr>
2022-08-04 09:59:57 +0200

    correct test ex3 to be portable on msvc and gcc

--- Test/test_backend/ex3.cpp
+++ Test/test_backend/ex3.cpp
@@ -12,11 +12,12 @@ alignas(32) float  array2[8]={2.0,15.0,8.0,8.0,4.0,2.0,3.0,1.0};
 __m256 op1= _mm256_loadu_ps(array1);
 __m256 op2= _mm256_loadu_ps(array2);
 __m256 result=_mm256_mul_ps(op1,op2);
-
+alignas(32) float res[8];
+_mm256_store_ps(res,result);
 
  for(int i=0;i<8;i++)
  {
-   std::cout << std::setprecision(23) << std::hexfloat << result[i] ;
+   std::cout << std::setprecision(23) << std::hexfloat << res[i] ;
    if(i!=7)
     std::cout <<" , ";
     else
