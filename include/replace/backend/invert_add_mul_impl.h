c2761993cc0bc202bcd5ea9a89956c41c6ebd4ca
melflitty <melflitty@aneo.fr>
2022-08-05 15:38:13 +0200

    remove useless spaces

--- include/replace/backend/invert_add_mul_impl.h
+++ include/replace/backend/invert_add_mul_impl.h
@@ -7,12 +7,7 @@ namespace pene
       struct invert_add_mul_float_impl
       {
         // the void* arg is for the interflop backen context.
-        static  void add_float(float a, float b, float* cptr, void*) noexcept 
-        { 
-    
-          *cptr = a * b;
-          
-          }// replace + with *
+        static  void add_float(float a, float b, float* cptr, void*) noexcept { *cptr = a * b; }// replace + with *
         static void sub_float(float a, float b, float* cptr, void*) noexcept { *cptr = a - b; }
         static void mul_float(float a, float b, float* cptr, void*) noexcept { *cptr = a + b; }// replace * with +
         static void div_float(float a, float b, float* cptr, void*) noexcept { *cptr = a / b; }
