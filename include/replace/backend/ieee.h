6cdb734ba3687265696748e2e51e1c41b03f7073
melflitty <melflitty@aneo.fr>
2022-08-05 15:32:22 +0200

    Backend functions; no need to be constexpr

--- include/replace/backend/ieee.h
+++ include/replace/backend/ieee.h
@@ -7,15 +7,15 @@ namespace pene
       struct ieee
       {
         // the void* arg is for the interflop backen context.
-        static constexpr void add_float(float a, float b, float* cptr, void*) noexcept { *cptr = a + b; }
-        static constexpr void sub_float(float a, float b, float* cptr, void*) noexcept { *cptr = a - b; }
-        static constexpr void mul_float(float a, float b, float* cptr, void*) noexcept { *cptr = a * b; }
-        static constexpr void div_float(float a, float b, float* cptr, void*) noexcept { *cptr = a / b; }
+        static void add_float(float a, float b, float* cptr, void*) noexcept { *cptr = a + b; }
+        static void sub_float(float a, float b, float* cptr, void*) noexcept { *cptr = a - b; }
+        static void mul_float(float a, float b, float* cptr, void*) noexcept { *cptr = a * b; }
+        static void div_float(float a, float b, float* cptr, void*) noexcept { *cptr = a / b; }
 
-        static constexpr void add_double(double a, double b, double* cptr, void*) noexcept { *cptr = a + b; }
-        static constexpr void sub_double(double a, double b, double* cptr, void*) noexcept { *cptr = a - b; }
-        static constexpr void mul_double(double a, double b, double* cptr, void*) noexcept { *cptr = a * b; }
-        static constexpr void div_double(double a, double b, double* cptr, void*) noexcept { *cptr = a / b; }
+        static void add_double(double a, double b, double* cptr, void*) noexcept { *cptr = a + b; }
+        static void sub_double(double a, double b, double* cptr, void*) noexcept { *cptr = a - b; }
+        static void mul_double(double a, double b, double* cptr, void*) noexcept { *cptr = a * b; }
+        static void div_double(double a, double b, double* cptr, void*) noexcept { *cptr = a / b; }
         static void* init() noexcept { return nullptr; }
       };
 
--- include/replace/backend/invert_add_mul_impl.h
+++ include/replace/backend/invert_add_mul_impl.h
@@ -7,30 +7,35 @@ namespace pene
       struct invert_add_mul_float_impl
       {
         // the void* arg is for the interflop backen context.
-        static constexpr void add_float(float a, float b, float* cptr, void*) noexcept { *cptr = a * b; }// replace + with *
-        static constexpr void sub_float(float a, float b, float* cptr, void*) noexcept { *cptr = a - b; }
-        static constexpr void mul_float(float a, float b, float* cptr, void*) noexcept { *cptr = a + b; }// replace * with +
-        static constexpr void div_float(float a, float b, float* cptr, void*) noexcept { *cptr = a / b; }
+        static  void add_float(float a, float b, float* cptr, void*) noexcept 
+        { 
+    
+          *cptr = a * b;
+          
+          }// replace + with *
+        static void sub_float(float a, float b, float* cptr, void*) noexcept { *cptr = a - b; }
+        static void mul_float(float a, float b, float* cptr, void*) noexcept { *cptr = a + b; }// replace * with +
+        static void div_float(float a, float b, float* cptr, void*) noexcept { *cptr = a / b; }
 
-        static constexpr void add_double(double a, double b, double* cptr, void*) noexcept { *cptr = a + b; }
-        static constexpr void sub_double(double a, double b, double* cptr, void*) noexcept { *cptr = a - b; }
-        static constexpr void mul_double(double a, double b, double* cptr, void*) noexcept { *cptr = a * b; }
-        static constexpr void div_double(double a, double b, double* cptr, void*) noexcept { *cptr = a / b; }
+        static void add_double(double a, double b, double* cptr, void*) noexcept { *cptr = a + b; }
+        static void sub_double(double a, double b, double* cptr, void*) noexcept { *cptr = a - b; }
+        static void mul_double(double a, double b, double* cptr, void*) noexcept { *cptr = a * b; }
+        static void div_double(double a, double b, double* cptr, void*) noexcept { *cptr = a / b; }
         static void* init() noexcept { return nullptr; }
       };
 
       struct invert_add_mul_impl
       {
         // the void* arg is for the interflop backen context.
-        static constexpr void add_float(float a, float b, float* cptr, void*) noexcept { *cptr = a * b; }// replace + with *
-        static constexpr void sub_float(float a, float b, float* cptr, void*) noexcept { *cptr = a - b; }
-        static constexpr void mul_float(float a, float b, float* cptr, void*) noexcept { *cptr = a + b; }// replace * with +
-        static constexpr void div_float(float a, float b, float* cptr, void*) noexcept { *cptr = a / b; }
+        static void add_float(float a, float b, float* cptr, void*) noexcept { *cptr = a * b; }// replace + with *
+        static void sub_float(float a, float b, float* cptr, void*) noexcept { *cptr = a - b; }
+        static void mul_float(float a, float b, float* cptr, void*) noexcept { *cptr = a + b; }// replace * with +
+        static void div_float(float a, float b, float* cptr, void*) noexcept { *cptr = a / b; }
 
-        static constexpr void add_double(double a, double b, double* cptr, void*) noexcept { *cptr = a * b; }// replace + with *
-        static constexpr void sub_double(double a, double b, double* cptr, void*) noexcept { *cptr = a - b; }
-        static constexpr void mul_double(double a, double b, double* cptr, void*) noexcept { *cptr = a + b; }// replace * with +
-        static constexpr void div_double(double a, double b, double* cptr, void*) noexcept { *cptr = a / b; }
+        static void add_double(double a, double b, double* cptr, void*) noexcept { *cptr = a * b; }// replace + with *
+        static void sub_double(double a, double b, double* cptr, void*) noexcept { *cptr = a - b; }
+        static void mul_double(double a, double b, double* cptr, void*) noexcept { *cptr = a + b; }// replace * with +
+        static void div_double(double a, double b, double* cptr, void*) noexcept { *cptr = a / b; }
         static void* init() noexcept { return nullptr; }
       };
 
