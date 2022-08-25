8ebe33fd8b344f837468574db3aaaeeb1e40a1df
melflitty <melflitty@aneo.fr>
2022-08-25 14:01:35 +0000

    revert accidental crash of source_filter.cpp file

--- src/pin_utils/source_filter.cpp
+++ src/pin_utils/source_filter.cpp
@@ -1,4 +1,4 @@
-#include "pin_utils/symbol_filters.h"
+#include "pin_utils/source_filter.h"
 #include "utils/safe_get_line.h"
 #include "utils/match.h"
 #include <pin.H>
@@ -8,68 +8,31 @@ namespace pene
 {
   namespace pin_utils
   {
-    BOOL symbol_filter_base::is_in_list(ADDRINT addr) const
-    {
-      IMG img = IMG_FindByAddress(addr);
-      if (IMG_Valid(img))
-      {
-        std::string img_name(IMG_Name(img));
-        std::string rtn_name(RTN_FindNameByAddress(addr));
-        return is_in_list(img_name, rtn_name);
-      }
-      return false;
+    BOOL source_filter_base::is_in_list(ADDRINT addr) const
+    { 
+
+      INT32 line_number, column;
+      std::string filename;
+      PIN_GetSourceLocation(addr, &column, &line_number, &filename);
+      return is_in_list(filename, line_number);
     }
 
-    BOOL symbol_filter_base::is_in_list(const std::string & img_name, const std::string & rtn_name) const
+    BOOL source_filter_base::is_in_list(const std::string& filename, INT32 line_number) const
     {
-      if (sym_list.empty() && wildcarded_sym_list.empty()) return false;
-      auto it = sym_list.find(img_name);
-      if (it != sym_list.end())
-      {
-        if (it->second.find("*") != it->second.end())
-        {
-          //std::cerr << "mapped symbols in list: " << img_name << "\t" << rtn_name << std::endl;
-          return true;
-        }
-        return it->second.find(rtn_name) != it->second.end();
-      }
-      
-      if (unmatched_img_names.find(img_name) != unmatched_img_names.end())
-      {
-        return false;
-      }
-      
-      for  (auto&& wilded_it : wildcarded_sym_list)
-      {
-        if (utils::match(wilded_it.first, img_name))
-        {
-          sym_list[img_name].insert(wilded_it.second.begin(), wilded_it.second.end());
-          bool res = false
-            || wilded_it.second.find("*") != wilded_it.second.end() 
-            || wilded_it.second.find(rtn_name) != wilded_it.second.end()
-            ;
-          if (res)
-          {
-            //std::cerr << "mapped symbols in list: " << img_name << "\t" << rtn_name << "\n";
-            return true;
-          }
-        }
-      }
-      unmatched_img_names.insert(img_name);
-      
-      return false;
+      if (line_list.empty()) return false;
+
+      auto it = line_list.find(filename);
+      return it != line_list.end() && (it->second.find(line_number) != it->second.end());
     }
 
-    symbol_filter_base::symbol_filter_base(const std::string& sym_lis_filename)
-      : sym_list()
-      , wildcarded_sym_list()
-      , unmatched_img_names()
+    source_filter_base::source_filter_base(const std::string& source_lis_filename)
+      : line_list()
     {
-      std::ifstream inputFileStream(sym_lis_filename.c_str(), std::ifstream::in);
+      std::ifstream inputFileStream(source_lis_filename.c_str(), std::ifstream::in);
       if (!inputFileStream.is_open())
       {
         std::string error_mess = "Could not open file ";
-        error_mess.append(sym_lis_filename);
+        error_mess.append(source_lis_filename);
 
         std::cerr << error_mess << std::endl;
         PIN_WriteErrorMessage(error_mess.c_str(), 1000, PIN_ERR_SEVERITY_TYPE::PIN_ERR_FATAL, 0);
@@ -80,7 +43,7 @@ namespace pene
       inputFileStream.close();
     }
 
-    VOID symbol_filter_base::load(std::istream& inputFileStream)
+    VOID source_filter_base::load(std::istream& inputFileStream)
     {
       size_t line_nb = 0;
       while (!inputFileStream.eof())
@@ -107,42 +70,39 @@ namespace pene
               exit(1);
             }
           }
-          auto filename = line.substr(0, pos); 
-          //std::replace(filename.begin(), filename.end(), '/', '\\');
+          auto filename = line.substr(0, pos);
+          std::replace(filename.begin(), filename.end(), '/', '\\');
           std::string first = filename;
-          std::string second = utils::trim(line.substr(pos + 1));
+          INT32 second = std::atoi(line.substr(pos + 1).c_str());
 
           std::cerr << "Loaded from file :" << second << " in " << first << std::endl;
-          if (filename.find('*') == std::string::npos)
-          {
-            sym_list[first].insert(second);
-          }
-          else
-          {
-            wildcarded_sym_list[first].insert(second);
-          }
+
+          line_list[first].insert(second);
+
         }
       }
     }
 
-    filter::check_status symbol_filter_base::check_trace() const { return filter::CHECK; }
-    filter::check_status symbol_filter_base::check_bbl() const { return filter::CHECK; }
-    filter::check_status symbol_filter_base::check_ins() const { return filter::CHECK; }
+    filter::check_status source_filter_base::check_trace() const { return filter::CHECK_NEST; }
+    filter::check_status source_filter_base::check_bbl() const { return filter::CHECK_NEST; }
+    filter::check_status source_filter_base::check_ins() const { return filter::CHECK_NEST; }
+    filter::check_status source_filter_base::is_instrumented(TRACE) const { return filter::CHECK_NEST; }
+    filter::check_status source_filter_base::is_instrumented(BBL) const { return filter::CHECK_NEST; }
 
-    symbol_include_filter::symbol_include_filter(const std::string& sym_lis_filename)
-      : symbol_filter_base(sym_lis_filename)
+    source_include_filter::source_include_filter(const std::string& sym_lis_filename)
+      : source_filter_base(sym_lis_filename)
     {}
 
-    filter::check_status symbol_include_filter::is_instrumented(ADDRINT addr) const
+    filter::check_status source_include_filter::is_instrumented(ADDRINT addr) const
     {
       return is_in_list(addr) ? filter::INSTRUMENT : filter::IGNORE;
     }
 
-    symbol_exclude_filter::symbol_exclude_filter(const std::string& sym_lis_filename)
-      : symbol_filter_base(sym_lis_filename)
+    source_exclude_filter::source_exclude_filter(const std::string& sym_lis_filename)
+      : source_filter_base(sym_lis_filename)
     {}
 
-    filter::check_status symbol_exclude_filter::is_instrumented(ADDRINT addr) const
+    filter::check_status source_exclude_filter::is_instrumented(ADDRINT addr) const
     {
       return is_in_list(addr) ? filter::IGNORE : filter::INSTRUMENT;
     }
